2ab1b5618c57daf4ea62bb92daabdd960671dabe
jjmau14 <jjmau14@gmail.com>
2018-02-06 09:46:02 -0700

    registry request task summary

--- src/main/java/cs455/overlay/wireformats/RegistryRequestsTrafficSummary.java
+++ src/main/java/cs455/overlay/wireformats/RegistryRequestsTrafficSummary.java
@@ -4,19 +4,21 @@ import java.io.IOException;
 
 public class RegistryRequestsTrafficSummary extends Event {
 
+    private byte type = Protocol.REGISTRY_REQUESTS_TRAFFIC_SUMMARY;
+
     @Override
     public byte[] pack() throws IOException {
-        return new byte[0];
+        return new byte[] { this.type };
     }
 
     @Override
-    public void craft(byte[] b) {
-
+    public void craft(byte[] data) {
+        this.type = data[0];
     }
 
     @Override
     public int getType(){
-        return 0;
+        return this.type;
     }
 
 }
