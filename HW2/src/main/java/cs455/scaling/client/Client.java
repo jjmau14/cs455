f96232cf56e688e02184a24e4028a8b4b4fde821
Josh Mau <jjmau14@gmail.com>
2018-02-19 11:11:59 -0700

    new

--- HW2/src/main/java/cs455/scaling/client/Client.java
+++ HW2/src/main/java/cs455/scaling/client/Client.java
@@ -1,10 +1,20 @@
 package cs455.scaling.client;
 
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class Client {
 
     private String serverHost;
     private int serverPort;
     private int messageRate;
+    private SocketChannel client;
+    private ByteBuffer buf;
 
     public Client(String serverHost, int serverPort, int messageRate) {
         this.serverHost = serverHost;
@@ -13,7 +23,31 @@ public class Client {
     }
 
     public void init() {
+        try {
+            client = SocketChannel.open(new InetSocketAddress("localhost", 5000));
+            buf = ByteBuffer.allocate(256);
 
+            System.out.println("Response: " + sendMessage("Hello"));
+        } catch (Exception e){
+
+        }
+
+    }
+
+    private String sendMessage(String message) {
+        buf = ByteBuffer.wrap(message.getBytes());
+        String response = null;
+        try {
+            client.write(buf);
+            buf.clear();
+            client.read(buf);
+            response = new String(buf.array());
+            System.out.println(Arrays.toString(buf.array()));
+            buf.clear();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return response;
     }
 
     public static void main(String[] args) {
--- HW2/src/main/java/cs455/scaling/server/Server.java
+++ HW2/src/main/java/cs455/scaling/server/Server.java
@@ -1,9 +1,22 @@
 package cs455.scaling.server;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
 public class Server {
 
     private int port;
     private int poolSize;
+    private ServerSocketChannel server;
+    private Selector selector;
+    private ByteBuffer buf;
 
     public Server(int port, int poolSize) {
         this.port = port;
@@ -11,7 +24,63 @@ public class Server {
     }
 
     public void init() {
+        try {
+            this.selector = Selector.open();
+            this.server = ServerSocketChannel.open();
+            this.server.bind(new InetSocketAddress("localhost", port));
+            this.server.configureBlocking(false);
+            this.server.register(selector, SelectionKey.OP_ACCEPT);
+            this.buf = ByteBuffer.allocate(256);
 
+            System.out.println("Server listening on " + server.getLocalAddress());
+
+            while(true) {
+                selector.select();
+                Iterator<SelectionKey> keys = selector.selectedKeys().iterator();
+
+                while(keys.hasNext()) {
+                    SelectionKey key = keys.next();
+
+                    if (key.isAcceptable()) {
+                        register(selector, server);
+                    }
+
+                    if (key.isReadable()) {
+                        readAndReply(buf, key);
+                    }
+
+                    System.out.println("KEY: " + key.interestOps());
+                    keys.remove();
+                }
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void register(Selector selector, ServerSocketChannel server) {
+        try {
+            SocketChannel client = server.accept();
+            client.configureBlocking(false);
+            client.register(selector, SelectionKey.OP_READ);
+        } catch (Exception e) {
+            ;
+        }
+    }
+
+    private static void readAndReply(ByteBuffer buf, SelectionKey key) {
+        try {
+            SocketChannel client = (SocketChannel) key.channel();
+            client.read(buf);
+            System.out.println("Received: " + new String(buf.array()));
+            buf.clear();
+            buf = ByteBuffer.wrap("hello there".getBytes());
+            client.write(buf);
+            buf.clear();
+        } catch (Exception e) {
+
+        }
     }
 
     public static void main(String[] args) {
