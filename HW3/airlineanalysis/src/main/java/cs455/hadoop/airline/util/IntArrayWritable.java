8401d6fb39e388160f575f898579b8f5f7681ec5
Josh Mau <jjmau14@gmail.com>
2018-04-01 18:43:54 -0600

    forgot to assign the values

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/util/IntArrayWritable.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/util/IntArrayWritable.java
@@ -1,20 +1,12 @@
 package cs455.hadoop.airline.util;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-import java.io.DataOutput;
-import java.io.IOException;
 
 public class IntArrayWritable extends ArrayWritable {
-    public IntArrayWritable(IntWritable[] intWritables) {
-        super(IntWritable.class);
+
+    public IntArrayWritable(IntWritable[] values) {
+        super(IntWritable.class, values);
     }
 
     @Override
@@ -23,9 +15,8 @@ public class IntArrayWritable extends ArrayWritable {
     }
 
     @Override
-    public void write(DataOutput arg0) throws IOException {
-        for(IntWritable data : get()){
-            data.write(arg0);
-        }
+    public String toString() {
+        IntWritable[] values = get();
+        return values[0].toString() + ", " + values[1].toString();
     }
 }
