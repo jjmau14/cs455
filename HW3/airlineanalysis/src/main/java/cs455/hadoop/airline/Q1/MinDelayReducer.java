5b824a9d474f6697cd9e5b33e3c395b45987676e
Josh Mau <jjmau14@gmail.com>
2018-04-02 09:19:55 -0600

    removed excess context writes

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Hashtable;
 
 public class MinDelayReducer extends Reducer<Text, Text, Text, Text> {
@@ -11,14 +12,16 @@ public class MinDelayReducer extends Reducer<Text, Text, Text, Text> {
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Hashtable<Integer, Integer> key_values = new Hashtable<>();
-        context.write(key, new Text());
+
         for(Text t : values){
 
             try {
                 String[] data = t.toString().split("|");
                 int data_key = Integer.parseInt(data[0]);
                 int data_value = Integer.parseInt(data[1]);
-                context.write(key, new Text(data_key + ": " + data_value));
+
+                context.write(key, new Text(Arrays.toString(data)));
+
                 if (key_values.containsKey(data_key)) {
                     key_values.replace(data_key, key_values.get(data_key).intValue() + data_value);
                 } else {
