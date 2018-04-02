64210de6552187484d6d20e188579390c756212f
Josh Mau <jjmau14@gmail.com>
2018-04-01 18:47:02 -0600

    idk

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/util/IntArrayWritable.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/util/IntArrayWritable.java
@@ -5,18 +5,8 @@ import org.apache.hadoop.io.IntWritable;
 
 public class IntArrayWritable extends ArrayWritable {
 
-    public IntArrayWritable(IntWritable[] values) {
-        super(IntWritable.class, values);
+    public IntArrayWritable() {
+        super(IntWritable.class);
     }
 
-    @Override
-    public IntWritable[] get() {
-        return (IntWritable[]) super.get();
-    }
-
-    @Override
-    public String toString() {
-        IntWritable[] values = get();
-        return values[0].toString() + ", " + values[1].toString();
-    }
 }
