cdbc773c156d0b90bbad6d3b8ae0133740b59096
Josh Mau <jjmau14@gmail.com>
2018-04-02 09:26:53 -0600

    removed excess context writes

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
@@ -22,7 +22,7 @@ public class MinDelayReducer extends Reducer<Text, Text, Text, Text> {
                 int data_key = Integer.parseInt(data[0]);
                 int data_value = Integer.parseInt(data[1]);
 
-                context.write(key, new Text(Arrays.toString(data)));
+                context.write(key, new Text(data_raw));
 
                 if (key_values.containsKey(data_key)) {
                     key_values.replace(data_key, key_values.get(data_key).intValue() + data_value);
