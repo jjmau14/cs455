529a9a4a59eea3fdd320841d62c0e6b989c97f3c
Josh Mau <jjmau14@gmail.com>
2018-04-02 16:35:23 -0600

    removed excess context writes

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
@@ -13,15 +13,11 @@ public class MinDelayReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        Hashtable<Integer, Integer> key_values = new Hashtable<>();
+        Hashtable<Integer, Integer> kv = new Hashtable<>();
 
         for(Text t : values){
-            context.write(key, new Text(t));
+            context.write(key, new Text(Arrays.toString(t.toString().split("|"))));
         }
-
-        int min_key = Integer.MAX_VALUE;
-        int min_value = Integer.MAX_VALUE;
-
 
     }
 
