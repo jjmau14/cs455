800ceb55aec03d7c485b948a04439a85b1742741
Josh Mau <jjmau14@gmail.com>
2018-04-02 16:32:51 -0600

    removed excess context writes

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
@@ -16,42 +16,13 @@ public class MinDelayReducer extends Reducer<Text, Text, Text, Text> {
         Hashtable<Integer, Integer> key_values = new Hashtable<>();
 
         for(Text t : values){
-
-            try {
-                String[] data = t.toString().split("\\|");
-
-                if (data.length > 1) {
-                    int data_key = Integer.parseInt(data[0]);
-                    int data_value = Integer.parseInt(data[1]);
-
-                    if (key_values.containsKey(data_key)) {
-                        key_values.replace(data_key, key_values.get(data_key).intValue() + data_value);
-                    } else {
-                        key_values.put(data_key, data_value);
-                    }
-                } else {
-
-                }
-
-            } catch (NumberFormatException nfe) {
-                // pass
-            }
-
+            context.write(key, new Text(t));
         }
 
         int min_key = Integer.MAX_VALUE;
         int min_value = Integer.MAX_VALUE;
 
-        Set<Integer> keys = key_values.keySet();
-        for (Integer i : keys) {
-            context.write(new Text("TEST" + i), new Text(i + ": " + key_values.get(i)));
-            if (key_values.get(i) < min_value) {
-                min_key = i;
-                min_value = key_values.get(i);
-            }
-        }
 
-        context.write(key, new Text("MIN: " + min_key + ": " + min_value + "\n"));
     }
 
 }
