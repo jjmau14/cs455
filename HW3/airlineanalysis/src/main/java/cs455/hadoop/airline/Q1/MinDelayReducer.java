2cec37ab2f4016ee14fc5c4995bdd0f5a73d3213
Josh Mau <jjmau14@gmail.com>
2018-04-03 17:21:01 -0600

    new

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
@@ -14,43 +14,42 @@ public class MinDelayReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        Hashtable<Integer, Integer> kv = new Hashtable<>();
+        Hashtable<Integer, Integer[]> kv = new Hashtable<>();
 
         for(Text t : values){
             String s = t.toString();
             String[] arr = s.split("\\|");
 
-            Integer dataKey = 0;
-            Integer dataValue = 0;
-
             try {
-                dataKey = Integer.parseInt(arr[0]);
-                dataValue = Integer.parseInt(arr[1]);
+                Integer dataKey = Integer.parseInt(arr[0]);
+                Integer dataValue = Integer.parseInt(arr[1]);
 
-            } catch (Exception e) {
+                if (kv.containsKey(dataKey)) {
+                    Integer[] newArr = kv.get(dataKey);
+                    newArr[0] += dataValue; // Increment Value
+                    newArr[1] += 1;         // Increment Counter
+                    kv.replace(dataKey, newArr);
+                } else {
+                    kv.put(dataKey, new Integer[] {dataValue, 0});
+                }
+            } catch (NumberFormatException nfe) {
 
             }
 
-            if (kv.containsKey(dataKey)) {
-                kv.replace(dataKey, kv.get(dataKey) + dataValue);
-            } else {
-                kv.put(dataKey, dataValue);
-            }
-            
         }
 
         Set<Integer> keys = kv.keySet();
         Iterator<Integer> iter = keys.iterator();
-        boolean setInit = true;
+
         int minKey = 0;
         int minVal = Integer.MAX_VALUE;
 
         while (iter.hasNext()) {
             Integer i = iter.next();
 
-            if (kv.get(i) < minVal) {
+            if (kv.get(i)[0]/kv.get(i)[1] < minVal) {
                 minKey = i;
-                minVal = kv.get(i);
+                minVal = kv.get(i)[0]/kv.get(i)[1];
             }
         }
 
