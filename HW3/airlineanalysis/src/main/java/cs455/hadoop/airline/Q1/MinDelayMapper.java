4999a1849f93c2667f5efec8efdf344256ae75f0
jjmau14 <jjmau14@gmail.com>
2018-04-05 08:18:49 -0600

    added cleanup to test

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayMapper.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayMapper.java
@@ -1,6 +1,7 @@
 package cs455.hadoop.airline.Q1;
 
 import cs455.hadoop.airline.util.IntArrayWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -11,7 +12,7 @@ public class MinDelayMapper extends Mapper<
         LongWritable,   /* Input Key */
         Text,           /* Input Value */
         Text,           /* Output Key */
-        Text            /* Output Value Type */
+        IntWritable     /* Output Value Type */
     >{
 
     @Override
@@ -32,24 +33,52 @@ public class MinDelayMapper extends Mapper<
          *
          * */
 
-        String time, day, month, delay;
+        String time, day, month;
+        int delay;
         try {
             time = Integer.toString(Integer.parseInt(line[5]) / 100);
-            day = line[3];
-            month = line[1];
-            delay = line[14];
+            day = getDay(Integer.parseInt(line[3]));
+            month = getMonth(Integer.parseInt(line[1]));
+            delay = Integer.parseInt(line[14]);
         } catch (Exception e){
             return;
         }
 
-        String TIME = time + "|" + delay;
-        String DAY = day + "|" + delay;
-        String MONTH = month + "|" + delay;
+        context.write(new Text(time), new IntWritable(delay));
+        context.write(new Text(day), new IntWritable(delay));
+        context.write(new Text(month), new IntWritable(delay));
 
-        context.write(new Text("Time"), new Text(TIME));
-        context.write(new Text("Day"), new Text(DAY));
-        context.write(new Text("Month"), new Text(MONTH));
+    }
 
+    private String getDay(int i) throws Exception {
+        switch(i) {
+            case 1: return "mon";
+            case 2: return "tue";
+            case 3: return "wed";
+            case 4: return "thu";
+            case 5: return "fri";
+            case 6: return "sat";
+            case 7: return "sun";
+            default: throw new Exception("Could not parse " + i + " into a day of week.");
+        }
+    }
+
+    private String getMonth(int i) throws Exception {
+        switch(i) {
+            case 1: return "jan";
+            case 2: return "feb";
+            case 3: return "mar";
+            case 4: return "apr";
+            case 5: return "may";
+            case 6: return "jun";
+            case 7: return "jul";
+            case 8: return "aug";
+            case 9: return "sep";
+            case 10: return "oct";
+            case 11: return "nov";
+            case 12: return "dec";
+            default: throw new Exception("Could not parse " + i + " into a month.");
+        }
     }
 
 }
--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q1/MinDelayReducer.java
@@ -5,25 +5,27 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class MinDelayReducer extends Reducer<Text, Text, Text, Text> {
 
+    private Map<String, Integer[]> kv = new HashMap<>();
+
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        Hashtable<Integer, Integer[]> kv = new Hashtable<>();
 
         for(Text t : values){
             String s = t.toString();
             String[] arr = s.split("\\|");
 
             try {
-                Integer dataKey = Integer.parseInt(arr[0]);
+                String dataKey = arr[0];
                 Integer dataValue = Integer.parseInt(arr[1]);
 
+
+                /**
+                 *  If map contains the certain day, or month... etc.
+                 * */
                 if (kv.containsKey(dataKey)) {
                     Integer[] newArr = kv.get(dataKey);
                     newArr[0] += dataValue; // Increment Value
@@ -38,23 +40,76 @@ public class MinDelayReducer extends Reducer<Text, Text, Text, Text> {
 
         }
 
-        Set<Integer> keys = kv.keySet();
-        Iterator<Integer> iter = keys.iterator();
+    }
 
-        int minKey = 0;
-        int minVal = Integer.MAX_VALUE;
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException{
+        Set<String> keys = kv.keySet();
 
-        while (iter.hasNext()) {
-            Integer i = iter.next();
+        String topTime = "";
+        int topTimeVal = Integer.MAX_VALUE;
 
-            if (kv.get(i)[0]/kv.get(i)[1] < minVal) {
-                minKey = i;
-                minVal = kv.get(i)[0]/kv.get(i)[1];
+        String topDay = "";
+        int topDayVal = Integer.MAX_VALUE;
+
+        String topMonth = "";
+        int topMonthVal = Integer.MAX_VALUE;
+
+        for (String key : keys) {
+            if (isDay(key)) {
+                if (kv.get(key)[0] < topDayVal) {
+                    topDayVal = kv.get(key)[0];
+                    topDay = key;
+                }
+            }
+            else if (isMonth(key)) {
+                if (kv.get(key)[0] < topMonthVal) {
+                    topMonthVal = kv.get(key)[0];
+                    topMonth = key;
+                }
+            }
+            else {
+                if (kv.get(key)[0] < topTimeVal) {
+                    topTimeVal = kv.get(key)[0];
+                    topTime = key;
+                }
             }
         }
 
-        context.write(key, new Text(minKey + ": Delay: " + minVal));
+        context.write(new Text("Time"), new Text(topTime + Integer.toString(topTimeVal)));
+        context.write(new Text("Day"), new Text(topDay + Integer.toString(topDayVal)));
+        context.write(new Text("Month"), new Text(topMonth + Integer.toString(topMonthVal)));
+    }
 
+    private boolean isDay(String d) {
+        switch (d) {
+            case "mon": return true;
+            case "tue": return true;
+            case "wed": return true;
+            case "thu": return true;
+            case "fri": return true;
+            case "sat": return true;
+            case "sun": return true;
+            default: return false;
+        }
+    }
+
+    private boolean isMonth(String m) {
+        switch (m) {
+            case "jan": return true;
+            case "feb": return true;
+            case "mar": return true;
+            case "apr": return true;
+            case "may": return true;
+            case "jun": return true;
+            case "jul": return true;
+            case "aug": return true;
+            case "sep": return true;
+            case "oct": return true;
+            case "nov": return true;
+            case "dec": return true;
+            default: return false;
+        }
     }
 
 }
