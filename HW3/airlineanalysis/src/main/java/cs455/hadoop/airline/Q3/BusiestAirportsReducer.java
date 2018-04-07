d28b53b362512656e2cac4966e6a56d9d0ad32f3
Josh Mau <jjmau14@gmail.com>
2018-04-06 19:37:20 -0600

    Q3 testing

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q3/BusiestAirportsReducer.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q3/BusiestAirportsReducer.java
@@ -4,10 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class BusiestAirportsReducer extends Reducer<
         Text,   /* Input Key Type */
@@ -15,6 +12,8 @@ public class BusiestAirportsReducer extends Reducer<
         Text,   /* Output Key Type */
         Text    /* Output Value Type */
     >{
+
+    private HashMap<String, Integer> counts = new HashMap<>();
 
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context)
@@ -24,9 +23,25 @@ public class BusiestAirportsReducer extends Reducer<
         for (Text t : values) {
             count += 1;
         }
-        context.write(key, new Text(Integer.toString(count)));
+
+        counts.put(key.toString(), count);
     }
 
-
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        ArrayList<String> top10Airports = new ArrayList<>();
+        for (String key : counts.keySet()) {
+            if (top10Airports.size() < 10) {
+                top10Airports.add(key);
+            } else {
+                top10Airports.add(key);
+                Collections.sort(top10Airports);
+                top10Airports.remove(10);
+            }
+        }
+        for (int i = 0 ; i < 10 ; i++) {
+            context.write(new Text(top10Airports.get(i)), new Text(Integer.toString(counts.get(top10Airports.get(i)))));
+        }
+    }
 
 }
