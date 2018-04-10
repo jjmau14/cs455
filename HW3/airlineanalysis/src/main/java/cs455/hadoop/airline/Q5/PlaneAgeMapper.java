07769f4e76b80200804a5915dbb21b82bca0f0c1
Josh Mau <jjmau14@gmail.com>
2018-04-09 21:58:11 -0600

    final

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q5/PlaneAgeMapper.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q5/PlaneAgeMapper.java
@@ -21,7 +21,7 @@ public class PlaneAgeMapper extends Mapper<
         String[] line = value.toString().split(",");
 
         // <tailnum, arrDelay>
-        context.write(new Text(line[10]), new Text(line[14]));
+        context.write(new Text(line[10]), new Text(line[14] + "|" + line[0]));
     }
 
 }
