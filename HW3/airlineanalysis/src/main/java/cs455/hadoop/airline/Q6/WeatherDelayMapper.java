ecd4f702a46826c705452d695ec4600e2247db1c
jjmau14 <jjmau14@gmail.com>
2018-04-10 10:57:26 -0600

    Efe

--- HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q6/WeatherDelayMapper.java
+++ HW3/airlineanalysis/src/main/java/cs455/hadoop/airline/Q6/WeatherDelayMapper.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import javax.swing.text.EditorKit;
 import java.io.IOException;
 
 public class WeatherDelayMapper extends Mapper<
@@ -20,8 +21,12 @@ public class WeatherDelayMapper extends Mapper<
         String[] line = value.toString().split(",");
 
         // <origin iata, weatherDelay>
-        if (Integer.parseInt(line[25]) > 0) {
-            context.write(new Text(line[16]), new Text(line[25]));
+        try {
+            if (Integer.parseInt(line[25]) > 0) {
+                context.write(new Text(line[16]), new Text(line[25]));
+            }
+        } catch (Exception e) {
+            ;
         }
     }
 
